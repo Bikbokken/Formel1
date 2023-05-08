10866ccf861133688dcd60ab11232154c6588d1b
Bikbokken <benjaminburek05@gmail.com>
2023-05-08 10:23:51 +0200

    mega sej login og sådan noget

--- Serverside/main.py
+++ Serverside/main.py
@@ -7,9 +7,17 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 
+origins = [
+   "http://localhost",
+   "http://localhost:8080",
+   "http://127.0.0.1",
+]
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=['*']
+   CORSMiddleware,
+   allow_origins=origins,
+   allow_credentials=True,
+   allow_methods=["*"],
+   allow_headers=["*"],
 )
 
 class Data:
@@ -112,7 +120,7 @@ def read_root():
     return data.data
 
 
-@app.post("/items")
+@app.post("/update")
 def read_item(info : ListItem):
     data.setData(info.dict())
     return {"response": "OK"}
